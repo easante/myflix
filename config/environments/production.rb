af2ea3c3cc3717dda1c02190aa764289bcbecdd4
Emmanuel Asante <emmanuela.asante@gmail.com>
2014-06-21 15:25:41 -0600

    Add mailgun to application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,19 +18,16 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  #config.action_mailer.delivery_method = :smtp
-  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.smtp_settings = {
-    address:              'smtp.mailgun.org',
-    port:                 587,
-    domain:               'sandbox0b37c9b1a71f4d2d98dfbd99302dd8df.mailgun.org',
-    #user_name:            ENV['gmail-user'],
-    #password:             ENV['gmail-password'],
-    user_name:            'emmanuel@sandbox0b37c9b1a71f4d2d98dfbd99302dd8df.mailgun.org',
-    password:             5z5-se77fe62,
+    address:              ENV['MAILGUN_SMTP_SERVER'],
+    port:                 ENV['MAILGUN_SMTP_PORT'],
+    domain:               'shielded-dawn-5656.herokuapp.com',
+    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+    password:             ENV['MAILGUN_SMTP_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  }
 
